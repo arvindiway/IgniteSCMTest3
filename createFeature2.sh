c9e2badec047eea67733054ced8eee19b96c3659
arvindiway <arvindiway@gmail.com>
2019-02-04 11:22:45 -0800

    test commit

--- createFeature2.sh
+++ createFeature2.sh
@@ -30,7 +30,7 @@ git pull origin develop --allow-unrelated-histories
 #git checkout -t -b develop origin/develop 
 #git checkout develop
 git merge --no-ff $SCM_FEATURE_NAME --allow-unrelated-histories -Xignore-all-space -Xtheirs -m "$SCM_COMMIT_NAME"
-git pull develop
+git pull origin develop
 git push -u origin develop
 
 #git fetch origin develop
