c5b348368a7b87dead1abdf7c2ddc3b048ca9f1f
svcIgnite <svcIgnite@kp.org>
2019-06-01 13:06:22 -0700

    feature to master comments

---
+++ createFeatureToDev.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+LOCAL_REPO_DIR="/Users/arvindvelumani/Feature4"
+SID="S231"
+DATE_WITH_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+
+SCM_COMMIT_NAME="test commit"
+SCM_FEATURE_NAME="feature_"$SID"_"$DATE_WITH_TIME
+cd ${LOCAL_REPO_DIR}
+git init .
+# add changes
+git add .
+git remote add origin https://arvindiway@github.com/arvindiway/IgniteSCMTest4.git/
+#git merge develop develop doesnt exist on local
+echo create local develop
+git checkout -b develop
+
+echo pull from remote develop brnch
+git pull origin develop --allow-unrelated-histories
+#git clone https://github.com/arvindiway/FeatureTest2.git
+#git remote add origin https://arvindiway:bd2c4ccdf3379733d90586216ebdf3bc5c32c627@github.com/arvindiway/FeatureTest2.git/
+git checkout -b $SCM_FEATURE_NAME
+echo sync with develop
+git merge develop
+
+git commit -a -m  "$SCM_COMMIT_NAME"
+# to add new remote repo git remote add origin https://arvindiway:bd2c4ccdf3379733d90586216ebdf3bc5c32c627@github.com/arvindiway/ignite-scm-test-1.git/
+
+git push origin $SCM_FEATURE_NAME
+
+echo checkout develop
+git checkout develop
+echo merge with feature branch
+git merge --no-ff $SCM_FEATURE_NAME --allow-unrelated-histories -Xignore-all-space -Xtheirs -m "$SCM_COMMIT_NAME"
+echo push develop
+git push -u origin develop
+# when merge conflict this push will fail
+#possible solution when create a new service so it will have separate repo meaning no other service will be in dev?
+#since only one person will work on a service there will not be merge conflicts between feature to dev?
